ed6b84f39e4b03d30ea8c457efcae474a5a4e78e
Sudeep Khandekar <83664116+sudeepkhandekar99@users.noreply.github.com>
2024-03-16 20:41:30 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -57,8 +57,6 @@ async def create_personal_info(payload: PersonalInfo_request):
         payload.batch = "Class 6"
     else:
         payload.batch = "Class 7"
-    total = await PersonalInfo.all().count() 
-    payload.activity = "Morning" if total <=150 else "Evening"
     payload.division = "A" if payload.sex == 'M' else "B"
     new_info = await PersonalInfo.create(**payload.dict())
     response  = await PersonalInfo_pydantic.from_tortoise_orm(new_info)
@@ -170,12 +168,21 @@ async def create_user(name: str, email: str, password: str):
 @app.put('/update_fees_status/{personal_info_id}')
 async def update_fees_status(personal_info_id: int):
     personal_info = await PersonalInfo.get_or_none(registration_number=personal_info_id)
+    
     if personal_info is None:
         raise HTTPException(status_code=404, detail="Personal Info not found")
+    
     personal_info.fees_status = True
+
+    if personal_info.summer_camp_id:
+        pass
+    else:
+        summer_camp_record = await SummerID.create(personal_info_id=personal_info_id)
+        await summer_camp_record.save()
+        personal_info.summer_camp_id = summer_camp_record.id
+    
     await personal_info.save()
-    updated_personal_info = await PersonalInfo_pydantic.from_tortoise_orm(personal_info)
-    return {"status": "success", "updated_info": updated_personal_info}
+    return {"status": "success", "updated_info": personal_info}
 
 
 register_tortoise(
